f7d5121bfab188e8153351f0521ad179c96b72c9
Laliga <jezs1604@gmail.com>
2024-04-29 12:41:04 -0700

    Se agrega el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InputDecorator } from '@angular/core';
+import { IndexComponent } from './core/index/index.component';
+import { ContacComponent } from './core/contac/contac.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        {path: "", title: "index", component: IndexComponent},
+        {path: "index", title: "index", component: IndexComponent}
+];
